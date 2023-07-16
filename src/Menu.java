71f18b07f2c3f04e7b89a36f162588eb6f397d1d
hieu080 <134911574+hieu080@users.noreply.github.com>
2023-07-16 16:07:16 +0700

    Update Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -14,20 +14,34 @@ import java.util.Scanner;
  * @param <E>
  */
 public class Menu<E> {
+    
+    //Phương thức này nhận đầu vào là một danh sách ArrayList (options) chứa các phần tử kiểu E và trả về một giá trị nguyên.
+    //Phương thức này sẽ hiển thị danh sách các phần tử trong options kèm theo số thứ tự của mỗi phần tử, 
+    //sau đó yêu cầu người dùng chọn một tùy chọn từ 1 đến số lượng tùy chọn có sẵn. 
+    //Sau khi người dùng chọn, phương thức trả về giá trị nguyên tương ứng với lựa chọn của người dùng.
     public int int_getChoice(ArrayList<E> options) {
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < options.size(); i++)
+        for (int i = 0; i < options.size(); i++) {
             System.out.println((i + 1) + " - " + options.get(i));
+        }
         System.out.print("Please choose an option 1 .. " + options.size() + ": ");
         return Integer.parseInt(sc.nextLine());
     }
+    
+    //Phương thức này nhận đầu vào là một danh sách ArrayList (options) chứa các phần tử kiểu E và trả về một phần tử kiểu E.
+    //Phương thức này hiển thị danh sách các phần tử trong options kèm theo số thứ tự của mỗi phần tử. 
+    //Sau đó, nó lặp lại việc yêu cầu người dùng chọn một tùy chọn bằng cách sử dụng phương thức int_getChoice. 
+    //Lặp lại quá trình này cho đến khi người dùng chọn một tùy chọn hợp lệ (từ 1 đến số lượng tùy chọn có sẵn). 
+    //Khi đó, phương thức trả về phần tử tương ứng với lựa chọn của người dùng.
     public E ref_getChoice(ArrayList<E> options) {
         int response;
-        for (int i = 0; i < options.size(); i++)
+        for (int i = 0; i < options.size(); i++) {
             System.out.println((i + 1) + " - " + options.get(i));
-        do 
+        }
+        do {
             response = int_getChoice(options);
-        while (response < 0 || response > options.size());
+        } while (response < 0 || response > options.size());
         return options.get(response - 1);
     }
 }
+
